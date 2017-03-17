27086f84dfd786c43980598e7f42a7666a735388
johan <johanloudon@gmail.com>
2017-03-17 12:05:06 +0100

    nothing

--- src/ga/Individual.java
+++ src/ga/Individual.java
@@ -5,14 +5,6 @@ package ga;
  */
 public class Individual {
 
-    int[] genoType;
-    int[][][] imageMatrix;
-
-    public Individual(int[][][] imageMatrix, int numberOfSegments) {
-        this.imageMatrix = imageMatrix;
-        int genoTypeSize = imageMatrix.length*imageMatrix[0].length;
-        genoType = new int[genoTypeSize];
-
-    }
+    
 
 }
